31c4e4e4e742ac8de25fdb139154d5c1c309f382
aayutha <thatheraaayushi@gmail.com>
2023-06-15 10:15:25 +0530

    bottomtab

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Bottomtab from './Bottomtab';
 const App = () => {
+  const Stack = createNativeStackNavigator();
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Bottomtab' screenOptions={{
+          headerShown:false
+        }} >
+      <Stack.Screen name='Bottomtab' component={Bottomtab}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
